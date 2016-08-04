d60ca86a3b23760f025929dd6eac359df2bbb94d
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2016-08-04 13:27:45 +0200

    added basic example site

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -35,6 +35,7 @@ ADMINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'website.apps.WebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
