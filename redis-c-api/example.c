e07ca3f1544012b54a9eee6a457da62a1042c8f5
Ramin Bahadoorifar <rbahadoorifar@student.tgm.ac.at>
2016-11-08 10:06:38 +0100

    updated example.c

--- redis-c-api/example.c
+++ redis-c-api/example.c
@@ -1,19 +1,19 @@
 #include "redis_api.c"
 
 void callback_example(const char key[], const char value[]) {
-	printf("%s ==== %s\n\n\n", key, value);
+	printf("%s ==== %s\n", key, value);
 }
 
 int main (int argc, char **argv) {
 	//############################
 	//SYNC (SET AND GET EXAMPLE)
 	//############################
-	redisContext *c = create_connection();
+	redisContext *c = create_connection("localhost", 6379);
 	//SETS THE TEMPERATURE TO 23
-	set(c,SENSOR_TEMPERATURE, "23");
+	set(c, "temperature 23 humidity 20");
 	//GETS THE VALUE OF TEMPERATURE
 	const char *temp = get(c, SENSOR_TEMPERATURE);
-	printf("TEMPRATURE IS %s\n", temp);
+	printf("TEMPERATURE IS %s \n", temp);
 	//DISCONNECTS FROM SERVER
 	disconnect_connection(c);
 
