1ac7fb11eaacc1f6933e93a032890241e8ee2789
rbahadoorifar-tgm <rbahadoorifar@student.tgm.ac.at>
2016-11-09 06:42:31 +0100

    added webapp client socket events

--- webapp/public/javascripts/index.js
+++ webapp/public/javascripts/index.js
@@ -1,3 +1,33 @@
-/**
- * Created by Ramin on 01.11.2016.
- */
+var socket;
+
+$(document).ready(function () {
+	socket = io.connect('http://127.0.0.1:8080', {
+		query: 'token=' + localStorage.getItem('jwt')
+	});
+
+	socket.on('error', function (message) {
+		console.log(message);
+	});
+
+	socket.on('success', function () {
+		console.log('success!!');
+	});
+
+	socket.on('jwt', function (token) {
+		localStorage.setItem('jwt', token);
+	});
+
+	socket.on('updateJSON', function (json) {
+		console.log('json updated');
+	});
+
+	$('#register').on('click', function () {
+		console.log('click');
+		socket.emit('register', $('#username').val(), $('#password').val());
+	});
+
+	$('#login').on('click', function () {
+		console.log('click');
+		socket.emit('login', $('#username').val(), $('#password').val());
+	});
+});
