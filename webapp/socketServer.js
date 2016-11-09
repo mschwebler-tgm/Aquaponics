1252d064a17b0e092c72341945c76c86905aa230
rbahadoorifar-tgm <rbahadoorifar@student.tgm.ac.at>
2016-11-09 06:44:27 +0100

    fixed misspellings

--- webapp/socketServer.js
+++ webapp/socketServer.js
@@ -3,19 +3,19 @@ var net = require('net');
 var verified_aquaponic_systems = [];
 var unverified_aquaponic_systems = [];
 
-const EVENT_AUTHENTIFICATION = 'authentification';
+const EVENT_AUTHENTICATION = 'authentication';
 const EVENT_DATA = 'data';
 
 var server = net.createServer(function (socket) {
 
-    unverified_aquaponic_systems.push(socket):
+    unverified_aquaponic_systems.push(socket);
 
     socket.on('data', function(data) {
         if(data.indexOf(EVENT_DATA) == 1) {
             //TODO: update on webapp and save in database
         }
-        else if(data.indexOf(EVENT_AUTHENTIFICATION) == 1) {
-            //TODO: verify authentification token
+        else if(data.indexOf(EVENT_AUTHENTICATION) == 1) {
+            //TODO: verify authentication token
         }
     });
 
