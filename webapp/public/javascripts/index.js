e74dcc286e0adf5c5f1d239f0f7a7b0221a6b51c
Ramin Bahadoorifar <rbahadoorifar@student.tgm.ac.at>
2016-11-14 12:36:58 +0100

    updated socket.io events client-side

--- webapp/public/javascripts/index.js
+++ webapp/public/javascripts/index.js
@@ -1,33 +1,34 @@
 var socket;
 
 $(document).ready(function () {
-	socket = io.connect('http://127.0.0.1:8080', {
-		query: 'token=' + localStorage.getItem('jwt')
-	});
+    socket = io.connect('http://127.0.0.1:8080', {
+        query: 'token=' + localStorage.getItem('jwt')
+    });
 
-	socket.on('error', function (message) {
-		console.log(message);
-	});
+    socket.on('failure', function (data) {
+        console.log(data.message);
+    });
 
-	socket.on('success', function () {
-		console.log('success!!');
-	});
+    socket.on('success', function () {
+        console.log('success!!');
+    });
 
-	socket.on('jwt', function (token) {
-		localStorage.setItem('jwt', token);
-	});
+    socket.on('jwt', function (data) {
+        localStorage.setItem('jwt', data.token);
+    });
 
-	socket.on('updateJSON', function (json) {
-		console.log('json updated');
-	});
+    socket.on('updateJSON', function (json) {
+        console.log('json updated');
+        console.log(json);
+    });
 
-	$('#register').on('click', function () {
-		console.log('click');
-		socket.emit('register', $('#username').val(), $('#password').val());
-	});
+    $('#register').on('click', function () {
+        console.log('click');
+        socket.emit('register', $('#username').val(), $('#password').val());
+    });
 
-	$('#login').on('click', function () {
-		console.log('click');
-		socket.emit('login', $('#username').val(), $('#password').val());
-	});
+    $('#login').on('click', function () {
+        console.log('click');
+        socket.emit('login', $('#username').val(), $('#password').val());
+    });
 });
