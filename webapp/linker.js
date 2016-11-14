18b21748b83d470afb037ad5177f5b749e2c372a
Ramin Bahadoorifar <rbahadoorifar@student.tgm.ac.at>
2016-11-14 12:38:38 +0100

    updated linker.js

--- webapp/linker.js
+++ webapp/linker.js
@@ -1,10 +1,28 @@
-var IOCLIENTS = {};
-var NETCLIENTS = {};
-
-var publishToNET = function(id, key, value) {
-	NETCLIENTS.id.write(key+ ':' + value);
+function Linker () {
+    this.IOCLIENTS = {};
+    this.NETCLIENTS = {};
 }
 
-var publishToIO = function(id, json) {
-	IOCLIENTS.id.emit('updateJSON', json);
+Linker.prototype.addIOClient = function(id, object) {
+    this.IOCLIENTS.add(id,object);
+}
+
+Linker.prototype.removeIOClient = function(id) {
+    this.IOCLIENTS.remove(id);
+}
+
+Linker.prototype.addNETClient = function(id, object) {
+    this.NETCLIENTS.add(id,object);
+}
+
+Linker.prototype.removeNETClient = function(id) {
+    this.NETCLIENTS.remove(id);
+}
+
+Linker.prototype.publishToNETClient = function(id, key, value) {
+    NETCLIENTS.id.write(key+ ':' + value);
+}
+
+Linker.prototype.publishToIOClient = function(id, json) {
+    IOCLIENTS.id.emit('updateJSON', json);
 }
