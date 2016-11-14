3b4b9e2254d1d917763d90c2b56699a5849e0b38
Ramin Bahadoorifar <rbahadoorifar@student.tgm.ac.at>
2016-11-14 12:36:04 +0100

    updated redis API

--- aquaponic-system/nodejs/redis_api.js
+++ aquaponic-system/nodejs/redis_api.js
@@ -1,23 +1,27 @@
-var ioredis = require('ioredis');
-var redis = new Redis();
-
+function Redis_API() {
+    this.ioredis = require('ioredis');
+    this.redis = new Redis();
+}
 /**
  * After receving a reply from redis, by HGETALL, the reply ['k1', 'v1', 'k2', 'v2'] will
  * be transformed into { k1: 'v1', 'k2': 'v2' }
  */
 Redis.Command.setReplyTransformer('hgetall', function (result) {
-	if (Array.isArray(result)) {
-		var obj = {};
-		for (var i = 0; i < result.length; i += 2) {
-			obj[result[i]] = result[i + 1];
-		}
-		return obj;
-	}
-	return result;
+    if (Array.isArray(result)) {
+        var obj = {};
+        for (var i = 0; i < result.length; i += 2) {
+            obj[result[i]] = result[i + 1];
+        }
+        return obj;
+    }
+    return result;
 });
 
 function getJSON(callback) {
-
+    redis.hgetall('system');
+    redis.exec(function(err, result) {
+       callback(result);
+    });
 }
 
 function publish(key, value) {
@@ -25,5 +29,6 @@ function publish(key, value) {
 }
 
 function subscribe(topic, callback) {
-
+    redis.subscribe(topic);
+    redis.on('message', callback);
 }
