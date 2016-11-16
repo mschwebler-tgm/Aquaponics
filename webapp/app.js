c7d821e67ff2415d52def5d23e340d1901cd8f5b
Ramin Bahadoorifar <rbahadoorifar@student.tgm.ac.at>
2016-11-16 13:44:36 +0100

    added linker, authentication, socketServer in app.js

--- webapp/app.js
+++ webapp/app.js
@@ -35,31 +35,42 @@ var users = require('./routes/users');
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
 var http = require('http');
 var server = http.createServer(app).listen(8080);
 var io = require('socket.io')(server);
-var authentication = require('./authentication')(io, app);
 
 // connect to database
 var dbConfig = require('./models/conf.js');
 var mongoose = require('mongoose');
 mongoose.connect(dbConfig.url);
+
+var linker = require('./linker');
+
+// init of authentication
+var Authentication = require('./authentication');
+var authentication = new Authentication(io,app)
+
+var socketServer = require('./socketServer')(linker, authentication);
+
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+// 	var err = new Error('Not Found');
+// 	err.status = 404;
+// 	next(err);
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+// 	// set locals, only providing error in development
+// 	res.locals.message = err.message;
+// 	res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+// 	// render the error page
+// 	res.status(err.status || 500);
+// 	res.render('error');
+// });
+
+
 
 module.exports = app;
