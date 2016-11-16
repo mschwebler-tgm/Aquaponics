0de065372974e9266bc00571b46ad0cd336ab620
Ramin Bahadoorifar <rbahadoorifar@student.tgm.ac.at>
2016-11-16 12:12:22 +0100

    Fixed Node.js REDIS-API (aquaponic-system)

--- aquaponic-system/nodejs/redis_api.js
+++ aquaponic-system/nodejs/redis_api.js
@@ -1,34 +1,54 @@
-function Redis_API() {
-    this.ioredis = require('ioredis');
-    this.redis = new Redis();
-}
-/**
- * After receving a reply from redis, by HGETALL, the reply ['k1', 'v1', 'k2', 'v2'] will
- * be transformed into { k1: 'v1', 'k2': 'v2' }
- */
-Redis.Command.setReplyTransformer('hgetall', function (result) {
-    if (Array.isArray(result)) {
-        var obj = {};
-        for (var i = 0; i < result.length; i += 2) {
-            obj[result[i]] = result[i + 1];
-        }
-        return obj;
-    }
-    return result;
-});
+var Redis = require('ioredis');
 
-function getJSON(callback) {
-    redis.hgetall('system');
-    redis.exec(function(err, result) {
-       callback(result);
-    });
-}
 
-function publish(key, value) {
+module.exports = function() {
+	/**
+	 * Should not be used for 'SUBSCRIBE'-commands.
+	 */
+	this.createConnection = function () {
+		return new Redis(6379, '192.168.0.16');
+	}
 
-}
+	/**
+	 * After receving a reply from redis, by HGETALL, the reply ['k1', 'v1', 'k2', 'v2'] will
+	 * be transformed into { k1: 'v1', 'k2': 'v2' }
+	 */
+	Redis.Command.setReplyTransformer('hgetall', function (result) {
+		if (Array.isArray(result)) {
+			var obj = {};
+			for (var i = 0; i < result.length; i += 2) {
+				obj[result[i]] = result[i + 1];
+			}
+			return obj;
+		}
+		return result;
+	});
 
-function subscribe(topic, callback) {
-    redis.subscribe(topic);
-    redis.on('message', callback);
+	this.getJSON = function (connection, callback) {
+		connection.hgetall('system').then(function (result) {
+			callback(result);
+		});
+	}
+
+	/**
+	 *
+	 * @param key
+	 * @param value
+	 */
+	this.publish = function (key, value) {
+
+	}
+
+	/**
+	 * Subscribes to given topic and executes callback if message was received.
+	 * Returns connection.
+	 * @param topic
+	 * @param callback function(channel, message)
+	 */
+	this.subscribe = function (topic, callback) {
+		var connection = this.createConnection();
+		connection.subscribe(topic);
+		connection.on('message', callback);
+		return connection;
+	}
 }
