e916b7e472ce4cd83551f6c978e4b2bc4aa3c34d
Ramin Bahadoorifar <rbahadoorifar@student.tgm.ac.at>
2016-11-16 12:20:05 +0100

    aquaponic-system can now send data to server

--- aquaponic-system/nodejs/socketClient.js
+++ aquaponic-system/nodejs/socketClient.js
@@ -1,23 +1,39 @@
 var net = require('net');
-var request = require('request');
-var localStorage = require('localStorage');
+var request = require('requestify');
+var REDISAPI = require('./redis_api');
+var Redis = new REDISAPI();
+//var localStorage = require('localStorage');
 
 
 const EVENT_AUTHENTICATION = 'authentication';
 const EVENT_DATA = 'data';
 
-var server = net.createServer(function (socket) {
+module.exports = function() {
 
-    unverified_aquaponic_systems.push(socket);
+	var client = new net.Socket();
+	var redis_connection = Redis.createConnection();
+	//TODO: INIT OF CLIENT EVENTS
 
-    socket.on('data', function(data) {
-        if(data.indexOf(EVENT_DATA) == 1) {
-            //TODO: update on webapp and save in database
-        }
-        else if(data.indexOf(EVENT_AUTHENTICATION) == 1) {
-            //TODO: verify authentication token
-        }
-    });
+	request.post('http://localhost:8080/authenticate', {
+		username: "test",
+		password: "test123"
+	}).then(function (response) {
+		var body = response.getBody();
+		console.log(body);
+		if (body.indexOf('ERROR') == -1) {
+			console.log('AUTHENTICATED!!');
+			client.connect(9090, "localhost", function () {
+					console.log('Connected To Server');
+				client.write(EVENT_AUTHENTICATION + '=' + body);
+			});
+			Redis.subscribe('system', function(channel, message) {
+				Redis.getJSON(redis_connection, function(result) {
+					console.log(result);
+					client.write(EVENT_DATA + '=' + JSON.stringify(result));
+				});
+			});
 
+		}
+	});
 
-});
+}
