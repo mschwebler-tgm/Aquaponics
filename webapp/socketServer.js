3dfe4b02f87082fb95c609c92c397f82c1a7ba52
Ramin Bahadoorifar <rbahadoorifar@student.tgm.ac.at>
2016-11-16 13:44:12 +0100

    socketServer can now receive data from aquaponic-systems

--- webapp/socketServer.js
+++ webapp/socketServer.js
@@ -1,23 +1,38 @@
 var net = require('net');
 
-var verified_aquaponic_systems = [];
-var unverified_aquaponic_systems = [];
-
 const EVENT_AUTHENTICATION = 'authentication';
 const EVENT_DATA = 'data';
 
-var server = net.createServer(function (socket) {
+module.exports = function(linker, authentication) {
+	var server = net.createServer(function (socket) {
 
-    unverified_aquaponic_systems.push(socket);
+		//TODO: SET TIMEOUT AND DISABLED IT AFTER SUCCESSFUL AUTHENTICATION
 
-    socket.on('data', function(data) {
-        if(data.indexOf(EVENT_DATA) == 1) {
-            //TODO: update on webapp and save in database
-        }
-        else if(data.indexOf(EVENT_AUTHENTICATION) == 1) {
-            //TODO: verify authentication token
-        }
-    });
+		socket.on('data', function (data) {
+			console.log('DATA EVENT FIRED');
+			data = "" + data;
+			if (data.indexOf(EVENT_DATA) != -1) {
+				//TODO: SAVE IN DATABASE (STATISTICS)
+				var json = data.substring(data.indexOf('=')+1);
+				console.log("CLIENT: " + socket._id + "\nSAYS: " + json);
+				if(socket._id != undefined) {
+					//linker.publishToIOClient(socket._id, json);
+				}
+			}
+			else if (data.indexOf(EVENT_AUTHENTICATION) != -1) {
+				var token = data.substring(data.indexOf('=')+1);
+				authentication.verifyToken( token, function(decodedToken) {
+					//TODO: DISABLE TIMEOUT
+					console.log('CLIENT AUTHENTICATED WITH ID: ' + decodedToken._id);
+					socket._id = decodedToken.id;
+					console.log('SOCKET ID SET TO ' + socket._id);
+					linker.addNETClient(decodedToken._id, socket);
+				});
+			}
+		});
 
 
-});
+	});
+
+	server.listen(9090, "localhost");
+}
