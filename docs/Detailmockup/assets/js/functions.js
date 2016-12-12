5298ae98b05ae10407b5ea70294dce37f87fb39f
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2016-12-12 13:05:38 +0100

    added detailed mockup for webapp

---
+++ docs/Detailmockup/assets/js/functions.js
@@ -0,0 +1,163 @@
+/**
+ * Created by matthias on 07.12.16.
+ */
+$(document).ready(function() {
+    /* Login */
+    $('#registerForm').fadeOut(100);
+    $('#login').click(function () {
+        $('#registerForm').fadeOut(0);
+        $('#loginForm').fadeIn(0);
+    })
+    $('#goToLogin').click(function () {
+        $('#loginmodal-submit').css("background-color: blue;")
+        $('#registerForm').fadeOut(100);
+        $('#loginForm').delay(100).fadeIn(100);
+    })
+    $('#goToRegister').click(function () {
+        $('#loginmodal-submit').css("background-color: green;")
+        $('#loginForm').fadeOut(100);
+        $('#registerForm').delay(100).fadeIn(100);
+    })
+
+    /* Menu-toggle */
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+
+        $('#display_advance').toggle('1000');
+        $("i", this).toggleClass("fa-plus fa-minus");
+    });
+
+    /*Scroll Spy*/
+    $('body').scrollspy({ target: '#spy', offset:80});
+
+    /*Smooth link animation*/
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    $('#stars').on('starrr:change', function(e, value){
+        $('#count').html(value);
+    });
+
+    $('#stars-existing').on('starrr:change', function(e, value){
+        $('#count-existing').html(value);
+    });
+
+});
+
+// Starrr plugin (https://github.com/dobtco/starrr)
+var __slice = [].slice;
+
+(function($, window) {
+    var Starrr;
+
+    Starrr = (function() {
+        Starrr.prototype.defaults = {
+            rating: void 0,
+            numStars: 5,
+            change: function(e, value) {}
+        };
+
+        function Starrr($el, options) {
+            var i, _, _ref,
+                _this = this;
+
+            this.options = $.extend({}, this.defaults, options);
+            this.$el = $el;
+            _ref = this.defaults;
+            for (i in _ref) {
+                _ = _ref[i];
+                if (this.$el.data(i) != null) {
+                    this.options[i] = this.$el.data(i);
+                }
+            }
+            this.createStars();
+            this.syncRating();
+            this.$el.on('mouseover.starrr', 'span', function(e) {
+                return _this.syncRating(_this.$el.find('span').index(e.currentTarget) + 1);
+            });
+            this.$el.on('mouseout.starrr', function() {
+                return _this.syncRating();
+            });
+            this.$el.on('click.starrr', 'span', function(e) {
+                return _this.setRating(_this.$el.find('span').index(e.currentTarget) + 1);
+            });
+            this.$el.on('starrr:change', this.options.change);
+        }
+
+        Starrr.prototype.createStars = function() {
+            var _i, _ref, _results;
+
+            _results = [];
+            for (_i = 1, _ref = this.options.numStars; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
+                _results.push(this.$el.append("<span class='glyphicon .glyphicon-star-empty'></span>"));
+            }
+            return _results;
+        };
+
+        Starrr.prototype.setRating = function(rating) {
+            if (this.options.rating === rating) {
+                rating = void 0;
+            }
+            this.options.rating = rating;
+            this.syncRating();
+            return this.$el.trigger('starrr:change', rating);
+        };
+
+        Starrr.prototype.syncRating = function(rating) {
+            var i, _i, _j, _ref;
+
+            rating || (rating = this.options.rating);
+            if (rating) {
+                for (i = _i = 0, _ref = rating - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+                    this.$el.find('span').eq(i).removeClass('glyphicon-star-empty').addClass('glyphicon-star');
+                }
+            }
+            if (rating && rating < 5) {
+                for (i = _j = rating; rating <= 4 ? _j <= 4 : _j >= 4; i = rating <= 4 ? ++_j : --_j) {
+                    this.$el.find('span').eq(i).removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+                }
+            }
+            if (!rating) {
+                return this.$el.find('span').removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+            }
+        };
+
+        return Starrr;
+
+    })();
+    return $.fn.extend({
+        starrr: function() {
+            var args, option;
+
+            option = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+            return this.each(function() {
+                var data;
+
+                data = $(this).data('star-rating');
+                if (!data) {
+                    $(this).data('star-rating', (data = new Starrr($(this), option)));
+                }
+                if (typeof option === 'string') {
+                    return data[option].apply(data, args);
+                }
+            });
+        }
+    });
+})(window.jQuery, window);
+
+$(function() {
+    return $(".starrr").starrr();
+});
