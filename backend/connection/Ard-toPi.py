0f3b5b4f841d40bf4400d52951d200e29ae56189
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2017-02-23 21:37:11 +0100

    data is now saved with a timestamp

--- backend/connection/Ard-toPi.py
+++ backend/connection/Ard-toPi.py
@@ -1,23 +1,77 @@
 import redis
 import serial
 import time
+import os
+import re
 
-# init serial
-# TODO 'ttyACM0' needs to be changed to whatever the raspberry Pi's USB-port is called
-ser = serial.Serial(
-    port='/dev/ttyACM0',
-    baudrate=115200
-)
 
-# init redis
-r = redis.StrictRedis(host='localhost', port=6380, db=0)
+def getKeyValue(data):
+    """"""
+    """ output when reading continously from serial port:
+    b'temperature 24.62\r\n'
+    b''
+    b''
+    b''
+    b'ph -30.56\r\n'
+    b''
+    ...
+    """
+    # extrude essential data: b'temperature 24.62\r\n'
+    #                         -> temperature 24.62
+    data = re.sub("['brn\\\]", "", data)
 
-data = ""
-while True:
-    bytesToRead = ser.inWaiting()
-    data = ser.read(bytesToRead)
-    if data != "":
-        kv = data.split(' ',data)   # kv... KeyValue
-        r.hset('system', kv[0], kv[1])   # write data to redis
-    time.sleep(0.1)
+    # split key and value
+    return data.split()
 
+
+# compile and upload .c file (execute commands from cli)
+# TODO
+
+try:
+    # init serial
+    ser = serial.Serial(
+        port='/dev/ttyACM0',
+        baudrate=115200
+    )
+
+    # get redis
+    r = redis.StrictRedis(host='localhost', port=6380, db=0)
+
+
+    cachingStarted = 0
+    while True:
+        # read from serial port
+        bytesToRead = ser.inWaiting()
+        data = str(ser.read(bytesToRead))
+
+        # sync input from serial so that "b'tp 25.50\r\n'" is not split into "b'tp " and "25.50\r\n"
+        if len(data) != 15 and len(data) != 16:
+            time.sleep(0.1)
+            continue
+
+        timestamp = str(int(time.time()))
+
+        # get all 3 kinds of data (temp, ph, ec)
+        kv_1 = getKeyValue(data)
+        time.sleep(0.1)
+        data = str(ser.read(bytesToRead))
+        kv_2 = getKeyValue(data)
+        time.sleep(0.1)
+        data = str(ser.read(bytesToRead))
+        kv_3 = getKeyValue(data)
+
+        # save to redisDB with timestamp
+        r.hmset(timestamp, {kv_1[0]: kv_1[1], kv_2[0]: kv_2[1], kv_3[0]: kv_3[1]})
+        r.publish('system', 'newData')
+except serial.SerialException as err:
+    print("Serial connection interface broken")
+    print(err)
+    # TODO set error message (redis) 1
+except redis.ConnectionError as err:
+    print('Redis server not reachable')
+    print(err)
+    # TODO set error message (redis) 2
+except Exception as err:
+    print("Unknown exception")
+    print(err)
+    # TODO set error message (redis) 3
